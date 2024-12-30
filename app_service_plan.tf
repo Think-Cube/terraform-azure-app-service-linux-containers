0925fe48564a0bccc75abfad9ff68abad7b034cd
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2024-12-30 12:46:40 +0000

    Update app_service_plan.tf

--- app_service_plan.tf
+++ app_service_plan.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service_plan" "main" {
-  name                = "${var.environment}-${var.app_service_name}-${var.region}-plan"
+  name                = "${var.environment}-${var.app_service_plan_name}${var.region}"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   kind                = var.app_service_plan_kind
